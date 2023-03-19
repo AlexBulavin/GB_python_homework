04ebabd7d7098d9cecac25d58db2321d152a5c8e
AlexBulavin <bulavin1970@icloud.com>
2023-03-19 20:17:50 +0300

    Complete task_04_list_and_dict

--- Lesson3/library.py
+++ Lesson3/library.py
@@ -50,22 +50,22 @@ class TypicalMethods:
                 sys.stdout.write('\a')
                 sys.stdout.flush()
 
-'''
-    // Метод для вывода бегущей строкой 
-     /// < summary > Метод для вывода бегущей строкой < / summary >
-    // / < param
-    name = "text" > Текст который нужно вывести в консоль < / param >
-    public static void OutputDynamicString(string text)
-    {
-        ForegroundColor = ConsoleColor.Blue;
-    for (int i = 0; i < text.Length; i++)
-    {
-        Thread.Sleep(1000 / text.Length);
-    Write(text[i]);
-    if (!OperatingSystem.IsMacOS()) Beep(Random.Shared.Next(37, 32767), 100);
-    }
-    ResetColor();
-    }'''
+    '''
+        // Метод для вывода бегущей строкой 
+         /// < summary > Метод для вывода бегущей строкой < / summary >
+        // / < param
+        name = "text" > Текст который нужно вывести в консоль < / param >
+        public static void OutputDynamicString(string text)
+        {
+            ForegroundColor = ConsoleColor.Blue;
+        for (int i = 0; i < text.Length; i++)
+        {
+            Thread.Sleep(1000 / text.Length);
+        Write(text[i]);
+        if (!OperatingSystem.IsMacOS()) Beep(Random.Shared.Next(37, 32767), 100);
+        }
+        ResetColor();
+        }'''
 
     def input_single_int(self):
         """
--- Lesson3/task_04_list_and_dict.py
+++ Lesson3/task_04_list_and_dict.py
@@ -32,19 +32,21 @@ def my_func_1(first_num, second_num, third_num):
 
 
 # ---------------- Вариант 2 ------------------
-def my_func_2(**data_num):
-    data_num = data_num.sort()
-
-    print(f'Сумма наибольших элементов равна {data_num[0] + data_num[1]}')
+def my_func_2(data_list):
+    print(f'Сумма наибольших элементов равна {data_list[0] + data_list[1]}')
 
 
 print(bcolors.OKBLUE)  # Задали цвет текста в консоли
-first_num = tm.input_single_int(f'Введите первое число')
-second_num = tm.input_single_int(f'Введите второе число')
-third_num = tm.input_single_int(f'Введите третье число')
+data_list = []
+first_num = tm.input_any_int(f'Введите первое число')
+data_list.append(first_num)
+second_num = tm.input_any_int(f'Введите второе число')
+data_list.append(second_num)
+third_num = tm.input_any_int(f'Введите третье число')
+data_list.append(third_num)
 
 my_func_1(first_num, second_num, third_num)
-
-my_func_2(first_num, second_num, third_num)
+data_list.sort(reverse=True)
+my_func_2(data_list)
 
 print(bcolors.ENDC)  # Возвратили цвет текста к исходному
