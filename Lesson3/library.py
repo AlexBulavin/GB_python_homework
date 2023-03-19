ae2afcc145a0dd6abb4f2607fb4a3ef7e0559c6b
AlexBulavin <bulavin1970@icloud.com>
2023-03-19 16:16:57 +0300

    Little fixes  task_01_list_and_dict

--- Lesson3/library.py
+++ Lesson3/library.py
@@ -11,6 +11,9 @@ import sys
 
 # Add class for coloring console text
 class bcolors:
+    """
+    Класс для вывода разноцветного текста в консоль
+    """
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
     OKGREEN = '\033[92m'
@@ -24,17 +27,22 @@ class bcolors:
 class TypicalMethods:
 
     def output_dynamic_string(self):
+        """
+        Метод имитирует печатную машинку
+        Выводит символы со скоростью 1/len(self)
+        :return:
+        """
         for i in range(len(self)):
-            time.sleep(1 / len(self))
-            sys.stdout.write(self[i])
-            sys.stdout.flush()
+            if self is not None:
+                time.sleep(1 / len(self))
+                sys.stdout.write(self[i])
+                sys.stdout.flush()
 
-            if not sys.platform.startswith('darwin'):
+            if not sys.platform.startswith('darwin'):  # Для Windows
                 frequency = random.randint(37, 32767)
                 duration = 100  # миллисекунды
                 sys.stdout.write('\a')
                 sys.stdout.flush()
-
 
     def input_single_int(self):
         """
@@ -48,14 +56,14 @@ class TypicalMethods:
 
         flag_input_ok = False
         while not flag_input_ok:
-            print(bcolors.OKBLUE)
+            print(bcolors.OKBLUE)  # Задали цвет текста в консоли
             try:
                 if (number_inputs := int(input(
-                                f'{TypicalMethods.output_dynamic_string(self)}'
-                                ))) > 0:
+                        f'{TypicalMethods.output_dynamic_string(self)}'
+                ))) > 0:
                     flag_input_ok = True
                     return number_inputs
-                    print(bcolors.ENDC)
+                    print(bcolors.ENDC)  # Возвратили цвет текста к исходному
                 else:
                     print(NOT_INT_POSITIVE)
             except ValueError or TypeError:
