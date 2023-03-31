901575156b01b7df3ed9bde49323894ce106401b
AlexBulavin <bulavin1970@icloud.com>
2023-04-01 00:06:42 +0300

    call to endpoint works with code 200

--- Lesson3/json_to_csv.py
+++ Lesson3/json_to_csv.py
@@ -1,15 +1,23 @@
 __author__ = 'Alex Bulavin'
+
 import json
+import os
+import ssl
 import datetime
 import csv
 import requests
+import sys
+
+sys.path.append('/Users/alex/Documents/Secutity/')
+from Секьюрити.p12_pass import password
+from query import query
 
 # Получаем от пользователя ID устройств
 device_ids = input("Введите ID устройств через запятую: ")
 device_ids = [int(device_id) for device_id in device_ids.split(",")]
 
 # Определяем параметры запроса
-query = """
+graphql_query = """
 query getRecipes($deviceIds: [Int!], $categoryId: Int, $descriptionIds: [Int!], $locale: String!, $recipeId: Int, $query: String, $offset: Int, $limit: Int) {
   recipe_description(
     distinct_on: [recipe_id]
@@ -45,7 +53,6 @@ query getRecipes($deviceIds: [Int!], $categoryId: Int, $descriptionIds: [Int!], 
         name
       }
     }
-
     recipe_ingredients {
             id
             state
@@ -66,8 +73,6 @@ query getRecipes($deviceIds: [Int!], $categoryId: Int, $descriptionIds: [Int!], 
                 }
             }
         }
-
-
     recipe_steps {
       id
       time
@@ -88,6 +93,22 @@ query getRecipes($deviceIds: [Int!], $categoryId: Int, $descriptionIds: [Int!], 
   }
 }
 """
+query_params = {
+    "device_id": device_ids,  # 16229,
+    "order_by_created_at": False,
+    "order_by_view_count": True,
+    "limit": 10,
+    "locale": "ru",
+    "offset": 0
+    # "deviceIds": device_ids,
+    # "categoryId": None,
+    # "descriptionIds": None,
+    # "locale": "ru_RU",
+    # "recipeId": None,
+    # "query": None,
+    # "offset": 0,
+    # "limit": 100
+}
 variables = {
     "deviceIds": device_ids,
     "categoryId": None,
@@ -98,28 +119,44 @@ variables = {
     "offset": 0,
     "limit": 100
 }
-data = {
-    "query": query,
-    "variables": variables
-}
+# data = {
+#     "query": query,
+#     "variables": variables
+# }
+
+# Создайте объект сессии
+session = requests.Session()
+p12_path = '/Users/alex/Downloads/client (5).p12'
+p12_password = password
+key_file = "/Users/alex/Documents/Secutity/Секьюрити/p12_pass.py"
+if not os.path.exists(key_file):
+    print(f'Error: Could not find the TLS key file at {key_file}')
+else:
+    print(f'TLS key file at {key_file} found')
+
+# Загрузите p12-сертификат в объект сессии
+session.cert = (p12_path, key_file)
 
 # Отправляем запрос на сервер
-url = 'https://cmsql.skydom.company/v1/graphql'
+url = 'https://content.dev.skydom.company/v1/graphql'  # 'https://cmsql.skydom.company/v1/graphql'
 headers = {'Content-type': 'application/json'}
 response = requests.post(url, headers=headers,
                          data=json.dumps({'query': query,
-                                          'variables': variables}),
-                         verify='/path/to/cert.pem')
-
-
-'''https://cmsql.readyforsky.com/content/devices/'''
+                                          'variables': variables}))
+print(f'requests.post at line 125 passed')
+# Отправьте запрос на сервер
+# response1 = session.post(url, json={'query': graphql_query, 'variables': query_params})
+# print(f'line 131 response1.status_code = {response1.status_code}')
+# '''https://cmsql.readyforsky.com/content/devices/'''
 # Проверяем успешность запроса
+print(f'line 134 response.status_code = {response.status_code}')
 if response.status_code == 200:
     # Преобразуем ответ в csv файл
     """# Открытие JSON-файла
     with open('/Users/alex/Documents/GeekBrains/GB_python_homework/lesson3/recipes.json', 'r') as f:
         data = json.load(f)"""
-    data = response.json()["data"]["recipe_description"]
+    print(f'line 146 response.json() = {response.json()["data"]["recipe_description"]}')
+    data = response.json()["data"]  # ["recipe_description"]
 
     # Открытие CSV-файла
     with open('file.csv', 'w', newline='') as f:
@@ -132,21 +169,29 @@ if response.status_code == 200:
                          'Количество', 'Размерность'])
 
         # Запись данных в CSV-файл
-        for recipe in data['data']['recipe_description']:
+        for recipe in data['recipe_description']:  # data['data']['recipe_description']:
             cooktime_seconds = recipe['cooktime']
             cooktime_minutes, cooktime_seconds = divmod(cooktime_seconds, 60)
             cooktime_hours, cooktime_minutes = divmod(cooktime_minutes, 60)
             cooktime_formatted = datetime.time(hour=cooktime_hours,
-                                               minute=cooktime_minutes)\
+                                               minute=cooktime_minutes) \
                 .strftime('%H:%M')
+            print(f'line178',
+                  recipe['id'],
+                  recipe['recipe']['translatable_fields'][0]
+                  ['name'],
+                  recipe['translatable_fields'][0]
+                  ['description'],
+                  recipe['energy'],
+                  cooktime_formatted, "", "", "")
             writer.writerow([
-                                recipe['id'],
-                                recipe['recipe']['translatable_fields'][0]
-                                ['name'],
-                                recipe['translatable_fields'][0]
-                                ['description'],
-                                recipe['energy'],
-                                cooktime_formatted, "", "", ""])
+                recipe['id'],
+                recipe['recipe']['translatable_fields'][0]
+                ['name'],
+                recipe['translatable_fields'][0]
+                ['description'],
+                recipe['energy'],
+                cooktime_formatted, "", "", ""])
             for ingredient in recipe['recipe_ingredients']:
                 if ingredient['quantity'] == 0:
                     text = "По вкусу"
--- Lesson3/query.py
+++ Lesson3/query.py
@@ -1,3 +1,8 @@
+__author__ = 'Alex Bulavin'
+
+# from json_to_csv import device_ids
+
+query = """
 query getRecipes($deviceIds: [Int!], $categoryId: Int, $descriptionIds: [Int!], $locale: String!, $recipeId: Int, $query: String, $offset: Int, $limit: Int) {
   recipe_description(
     distinct_on: [recipe_id]
@@ -75,3 +80,20 @@ query getRecipes($deviceIds: [Int!], $categoryId: Int, $descriptionIds: [Int!], 
     }
   }
 }
+"""
+
+variables = {
+    "deviceIds": None, #device_ids,
+    "categoryId": None,
+    "descriptionIds": None,
+    "locale": "ru_RU",
+    "recipeId": None,
+    "query": None,
+    "offset": 0,
+    "limit": 100
+}
+
+url = 'https://cmsql.skydom.company/v1/graphql'
+headers = {'Content-type': 'application/json'}
+
+path_to_cert = '/Users/alex/Documents/Python_projects/tubeles/venv/lib/python3.9/site-packages/future/backports/test/ssl_cert.pem'
