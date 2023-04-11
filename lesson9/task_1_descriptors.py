36f10fb2bae7dff7dc70b9196729671dd573f339
AlexBulavin <bulavin1970@icloud.com>
2023-04-11 07:46:33 +0300

    Add name type validation

--- lesson9/task_1_descriptors.py
+++ lesson9/task_1_descriptors.py
@@ -29,11 +29,36 @@ class PositiveNumber:
     def __set_name__(self, owner, incoming_attr):
         self.incoming_attr = incoming_attr
 
+class NameType:
+    # def __init__(self, incoming_attr):
+    #     self.incoming_attr = incoming_attr
+
+    def __get__(self, instance, owner):  # Здесь
+        # self - экземпляр класса PositiveNumber
+        # instance - это экземпляр вызывающего класса, например, Person
+        # owner - сам вызывающий класс, например, ivan
+        return instance.__dict__[self.incoming_attr]  # Все атрибуты объекта
+        # хранятся в словаре
+        # Здесь мы не изменили поведение __get__, на выход метода отдали
+        # входящее значение return instance.__dict__[self.incoming_attr]
+
+    def __set__(self, instance, value):
+        if type(value) != str:
+            raise ValueError("Тип данных должен быть str")
+        instance.__dict__[self.incoming_attr] = value  # Можно не писать эту
+        # строку
+
+    def __delete__(self, instance):
+        del instance.__dict__[self.incoming_attr]
+
+    def __set_name__(self, owner, incoming_attr):
+        self.incoming_attr = incoming_attr
 
 class Person:
     age = PositiveNumber()  # Дескриптор = атрибут объекта со "связанным"
     # поведением
     phone = PositiveNumber()
+    name = NameType()
 
     def __init__(self, name, age, phone):
         self.name = name
@@ -42,17 +67,20 @@ class Person:
 
 
 class BankAccount:
-    balance = PositiveNumber()
+    balance = PositiveNumber()  # Сопоставление идёт по имени, аргумент
+    # передавать не нужно
     card_number = PositiveNumber()
+    cardholder_name = NameType()
 
-    def __init__(self, balance, card_number):
+    def __init__(self, balance, card_number, cardholder_name):
         self.balance = balance
         self.card_number = card_number
+        self.cardholder_name = cardholder_name
 
 
 ivan = Person('Иван', 25, 123456)
 stephan = Person('Степан', 23, 6589)
 ivan.age = 15
 
-ivan_bank_account = BankAccount(50000, 123456789)
-stephan_bank_account = BankAccount(80000, 236547852)
+ivan_bank_account = BankAccount(50000, 123456789, 123)
+stephan_bank_account = BankAccount(80000, 236547852, "Stephan")
